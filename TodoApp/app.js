24dfd2639a3dfef7fdbca8882fc67f55c6dfe6b0
Anumjawaid <anumjawaid99@gmail.com>
2020-07-23 23:57:31 +0500

    delete functionality

--- TodoApp/app.js
+++ TodoApp/app.js
@@ -4,6 +4,11 @@ function Addtask(){
     //console.log(inp.value)//value of id is printed on console
     var li = document.createElement('li')
     var litext=document.createTextNode(inp.value)
+    //deletebtn
+    var dbtn=document.createElement('button')
+    var detext=document.createTextNode('DELETE')
+    dbtn.appendChild(detext)
+    li.appendChild(dbtn)
     li.appendChild(litext)
     list.appendChild(li)
     inp.value=''

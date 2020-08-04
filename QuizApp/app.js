b76f9adfe41d14be59dbb6550039480f02840717
Anumjawaid <anumjawaid99@gmail.com>
2020-08-04 20:22:34 +0500

    not working correctly

---
+++ QuizApp/app.js
@@ -0,0 +1,16 @@
+function nameshow(){
+    document.getElementById("start").style.display="none"
+    document.getElementById("name").style.display='inline'
+
+}
+function contshow(){
+    document.getElementById("cont").style.display='inline'
+    // if(document.getElementById("cont").style.display==='inline')
+    // {
+    // document.getElementById("secmain").style.display="inline"
+    // }
+    // else{
+    // document.getElementById("cont").style.display='inline'
+
+    // }
+}

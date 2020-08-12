c53c5f440ea9545affb25fa25138322a71be559f
Anumjawaid <anumjawaid99@gmail.com>
2020-08-12 21:25:26 +0500

    functionality

---
+++ Enfrasoft(Client)/worket/app.js
@@ -0,0 +1,8 @@
+alert("sdfgh")
+function addproject(){
+    document.getElementById('maincont').style.display='none'
+    document.getElementById('projcont').style.display='block'
+}
+function createproj(){
+    
+}

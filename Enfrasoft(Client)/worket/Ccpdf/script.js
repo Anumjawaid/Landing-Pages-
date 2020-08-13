d43107d4b364f482df9c219cedf6b2af20571499
Anumjawaid <anumjawaid99@gmail.com>
2020-08-13 17:29:53 +0500

    workflow shift

---
+++ Enfrasoft(Client)/worket/Ccpdf/script.js
@@ -0,0 +1,18 @@
+alert("sdsdfg")
+function exp(){
+    document.getElementById('drawer').style.display='inline-block'
+    document.getElementById('togg').style.display='none'
+    document.getElementById('cross').style.display='block'
+
+
+
+
+}
+function reexp(){
+    document.getElementById('drawer').style.display='none'
+    document.getElementById('togg').style.display='block'
+    document.getElementById('cross').style.display='none'
+
+
+
+}

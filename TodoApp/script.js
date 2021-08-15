a40cfc9b08afefa3992fea579fc9b59547de1d8d
Anumjawaid <anumjawaid99@gmail.com>
2021-08-15 23:39:53 +0500

    added ,deleted function

--- TodoApp/script.js
+++ TodoApp/script.js
@@ -1,13 +1,31 @@
 // Usage of TocreateElement
-var anchor=document.getElementById('maincontent')
+var anchor=document.getElementById('content')
 var task=document.getElementById('task')
+var taskslist=[]
 function gettask(){
-    var div=TocreateElement('div',task.value,anchor,'class',"taskslist",()=>change())
+    var div=TocreateElement('li',task.value,anchor,'class',"taskslist","none")
     console.log(div)
+    
+   var stat= TocreateElement('input',"status",div,"type",'radio',"chcktask()")
+    var upd=TocreateElement('button',"Update",div,"class",'btn upd','updtask()')
+    var del=TocreateElement('button',"Delete",div,"class",'btn del',"deltask(this)")
+    // upd.setAttribute('onclick',)
+    taskslist.push(task.value)
     task.value=""
-
+console.log(taskslist)
     
 }
+// function delotask(){
+//     console.log("hhhh")
+// }
+
+function deltask(param){
+    console.log(param.parentNode)
+    param.parentNode.innerHTML=""
+
+
+}
+
 function TocreateElement(elementname,elementtext,where,attrname,attr,funcname){
     // funcname=alert("nothing in the function")
     // you need to just pass the name of the element in the first parameter
@@ -20,7 +38,7 @@ function TocreateElement(elementname,elementtext,where,attrname,attr,funcname){
     element.appendChild(text)
     where.append(element)
     element.setAttribute(attrname,attr)
-    element.setAttribute('Onclick',funcname)
+    element.setAttribute('onclick',funcname)
     return element
 }
 function change(){
